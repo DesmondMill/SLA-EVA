b04211c32592985845c57c32bd9c0e68a8bdfa70
DesmondMill <DesmondMill@users.noreply.github.com>
2026-06-19 01:47:30 +0000

    Cleanup: remove compiled files from src/

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import 'reflect-metadata';
-const app = express();
-app.use(express.json());
-app.get('/health', (req, res) => {
-    res.json({ status: 'ok' });
-});
-export default app;
-//# sourceMappingURL=app.js.map
